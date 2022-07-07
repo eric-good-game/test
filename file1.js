af0e1b4e362090168a589b2b0fc4cf16129d4520
Eric Good Game <yourgitemail@example.com>
2022-07-07 07:22:27 -0600

    Update file1 (3)

--- file1.js
+++ file1.js
@@ -5,3 +5,21 @@ const getRandomNumber = (limit) => {
 }
 
 getRandomNumber(100);
+
+const isPrimeNumber = n => {
+  if (n==2) {
+    console.log(false);
+    return false
+  }
+  for(let i=2;i<n;i++){
+    if(!(n%i)){
+      console.log(false);
+      return false
+    }
+  }
+  console.log(true);
+  return true
+}
+
+isPrimeNumber(2);
+isPrimeNumber(3);
